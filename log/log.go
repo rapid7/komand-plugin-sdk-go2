81a72ee0975af2a3751866464ce06467c1f33ffb
Sean Kelly <sean@komand.com>
2017-03-23 09:27:38 -0400

    Logger changes

--- log/log.go
+++ log/log.go
@@ -1,4 +1,4 @@
-// package log is a simple logger abstraction that buffers internally, and writes out at a user-defined period
+// Package log is a simple logger abstraction that buffers internally, and writes out at a user-defined period
 // this enables us to log inside of an action, but delay where and when the log is written to an external source
 // so that the http server and the "one and done" old school action invocations can use the same log, but present
 // data back to the user in a simple manner.
@@ -8,34 +8,63 @@ import (
 	"bytes"
 	"fmt"
 	"io"
+	"log"
 )
 
-// Logger is a wrapper around a []string buffer. It is threadsafe questionmarks?
-type Logger struct {
+// Logger is a very lightweight interface around 2 simple log calls
+type Logger interface {
+	Println(string)
+	Printf(string, ...interface{})
+}
+
+// BufferedLogger is a wrapper around a []string buffer. It is threadsafe questionmarks?
+type BufferedLogger struct {
 	buf bytes.Buffer
 }
 
-// NewLogger will return a new instance of Logger
-func NewLogger() *Logger {
-	return &Logger{}
+// NewBufferedLogger will return a new instance of Logger
+func NewBufferedLogger() *BufferedLogger {
+	return &BufferedLogger{}
 }
 
-// Info is a simple logging function which appends the log lines to the data
-func (l *Logger) Info(line string) {
+// Println prints a line
+func (l *BufferedLogger) Println(line string) {
 	l.buf.WriteString(line + `\n`)
 }
 
-// Infof is the format enabled version of Info
-func (l *Logger) Infof(line string, vals ...interface{}) {
-	l.buf.WriteString(fmt.Sprintf(line, vals...) + `\n`)
+// Printf prints an f
+func (l *BufferedLogger) Printf(line string, vals ...interface{}) {
+	l.buf.WriteString(fmt.Sprintf(line, vals...))
 }
 
 // Flush will flush the logger to the provided io.Writer. This could be stdout, stderr, a string builder, etc.
-func (l *Logger) Flush(w io.Writer) (int64, error) {
+func (l *BufferedLogger) Flush(w io.Writer) (int64, error) {
 	return l.buf.WriteTo(w)
 }
 
 // String will implement the Stringer interface and also return a copy of the log data as a string
-func (l *Logger) String() string {
+func (l *BufferedLogger) String() string {
 	return l.buf.String()
+}
+
+// NormalLogger is just a passthrough to calling log.X
+// The reason this exists instead of just using the builtin logger as an interface
+// is that the Println signature takes a variadic of interfaces{} which i can't easily
+// turn into strings for the buffer in the buffered logger. So, it's a little bit of
+// fuffery to make things easier for the real purpose of the logger, which is buffering.
+type NormalLogger struct{}
+
+// NewNormalLogger returns a new normal logger
+func NewNormalLogger() *NormalLogger {
+	return &NormalLogger{}
+}
+
+// Println prints a line
+func (l *NormalLogger) Println(line string) {
+	log.Println(line)
+}
+
+// Printf prints an f
+func (l *NormalLogger) Printf(line string, vals ...interface{}) {
+	log.Printf(line, vals...)
 }
