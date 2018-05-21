f45f9aa098b8265fef8ecd0050b4cb67b4657e5f
Stabby <seanm_kelly@rapid7.com>
2018-05-21 17:33:21 -0400

    Adding in support for credential structures

--- type_mapper.go
+++ type_mapper.go
@@ -1,27 +1,37 @@
 package sdk
 
-import "strings"
+import (
+	"log"
+	"strings"
+)
 
 // TypeMapper handles renaming types from the spec when they don't line up with go types by name
 type TypeMapper struct {
-	CustomTypes map[string]string
+	CustomTypes    map[string]string
+	StructureTypes map[string]string
 }
 
 // NewTypeMapper returns a new TypeMapper
 func NewTypeMapper(s *PluginSpec) *TypeMapper { // TO FIGURE OUT object and bytes need some work, look at generate/conversion
 	t := &TypeMapper{
 		CustomTypes: map[string]string{
-			"boolean":   "bool",
-			"object":    "interface{}",
-			"integer":   "int",
-			"bytes":     "[]byte",
-			"date":      "time.Time",
-			"date-time": "time.Time",
-			"number":    "float64",
-			"float":     "float64",
-			"python":    "string",
-			"password":  "string",
-			"file":      "types.SDKFile",
+			"boolean":    "bool",
+			"object":     "interface{}",
+			"integer":    "int",
+			"bytes":      "[]byte",
+			"date":       "time.Time",
+			"date-time":  "time.Time",
+			"number":     "float64",
+			"float":      "float64",
+			"python":     "string",
+			"password":   "string",
+			"file":       "types.SDKFile",
+			"credential": "credential",
+		},
+		StructureTypes: map[string]string{
+			"token":          "types.CredentialToken",
+			"passsword":      "types.CredentialPassword",
+			"asymmetric_key": "types.CredentialAsymmetricKey",
 		},
 	}
 
@@ -32,17 +42,22 @@ func NewTypeMapper(s *PluginSpec) *TypeMapper { // TO FIGURE OUT object and byte
 }
 
 // SpecTypeToGoType maps spec types to go types
-func (t *TypeMapper) SpecTypeToGoType(specType string) string {
+func (t *TypeMapper) SpecTypeToGoType(pd ParamData) string {
 	var isArr bool
-	strippedType := specType
-	if strings.HasPrefix(specType, "[]") {
-		strippedType = specType[2:]
+	strippedType := pd.Type
+	if strings.HasPrefix(pd.Type, "[]") {
+		strippedType = pd.Type[2:]
 		isArr = true
 	}
 	foundType, ok := t.CustomTypes[strippedType]
 	if !ok {
-		// We don't have a remap for it, so use the original
-		return specType
+		return pd.Type
+	}
+	if foundType == "credential" {
+		if foundType, ok = t.StructureTypes[pd.Structure]; !ok {
+			log.Printf("Cannot find Structure %s for type credential", pd.Structure)
+			return pd.Type
+		}
 	}
 	if isArr {
 		foundType = "[]" + foundType
